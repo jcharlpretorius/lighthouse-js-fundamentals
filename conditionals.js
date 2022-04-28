d45ee4c0fb8498380f85230cbec081549b726d22
jcharlpretorius <pretorius.jeancharl@gmail.com>
2022-04-28 16:23:14 -0600

    Write more comments and tidy up code

--- conditionals.js
+++ conditionals.js
@@ -2,10 +2,16 @@
 const raining = false;
 const temperature = 33;
 
+// Reminder to take umbrella with if it is raining
 if (raining) {
   console.log("Don't forget an umbrella!");
 }
+// use ! operator, tells you to leave umbrella at home when not raining
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
 
+// Gives advice on what to wear outside depending on the weather
 if (temperature < 0) {
   console.log("Make sure you pick out a scarf!");
 } else if (temperature < 15) {
@@ -17,7 +23,7 @@ console.log("Now you're ready to go outside!");
 
 const isCitizen = true;
 const age = 26;
-
+// Checks your eligibility to vote
 if (isCitizen && age > 18) {
   console.log("You are eligible to vote.");
 }
@@ -27,6 +33,3 @@ if (temperature < -40 || temperature > 40) {
   console.log("Maybe going outside isn't such a good idea...");
 }
 
-if (!raining) {
-  console.log("Leave your umbrella at home!");
-}
