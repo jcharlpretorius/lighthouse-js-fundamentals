18ba4c256736cd8fd89b9f192b0e252497cabe62
jcharlpretorius <pretorius.jeancharl@gmail.com>
2022-05-01 15:00:00 -0600

    Use loop methods to loop over arrays

---
+++ packing-list.js
@@ -0,0 +1,7 @@
+
+const packingList = ["bowls", "plates", "pot", "pans", "eating utensils", "glasses", "cups", "cooking gutensils"];
+
+console.log("Kitchen stuff to pack:");
+for (let i= 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
